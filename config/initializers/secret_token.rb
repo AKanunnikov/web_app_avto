d68f67dffa8a6638e3cd7de08c09864082a5cbfa
AKanunnikov <wolfheart2511@gmail.com>
2014-07-04 23:52:09 +0400

    Added a Pages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebAppAvto::Application.config.secret_token = 'c682f951b5859b2dd47482cdb8815acd0c27f3a5fbc97b5bce0d61d71d6e29f874d9cd0c4dd7f1caf0608c0c3750c035066017b6580430a301bbb7da6ed897d4'
